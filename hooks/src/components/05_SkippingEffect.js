5924aa9b08a9c8a6c228f2dd61f3c2fbc7a2e81c
Loic CHETY <lchety@e1r10p13.42.fr>
2019-08-15 14:58:51 +0200

    wip

--- hooks/src/components/05_SkippingEffect.js
+++ hooks/src/components/05_SkippingEffect.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from 'react';
 function SkippingEffect(){
 
 	const [click, setClick] = useState(0);
+	const [hello, setHello] = useState(0);
 
 	useEffect(() => {
 		console.log("05 SkippingEffect : UseEffect !");
@@ -13,7 +14,8 @@ function SkippingEffect(){
 	return (
 		<div>
 			<h2>05 SkippingEffect</h2>
-			<button onClick={() => setClick(click)}>Click</button>
+			<button onClick={() => setHello(hello + 1)}>Hello</button>{/*if click here change only hello but no change for click, useEffect will not be called*/}
+			<button onClick={() => setClick(click + 1)}>Click</button> {/*if click here change click so useEffect will be called*/}
 		</div>
 	);
 
