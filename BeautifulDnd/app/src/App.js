324dfccdd2d685e93f6e86149c30a26e326a4c7d
Dormin <loic.chety@gmail.com>
2019-11-03 16:31:39 +0100

    wip

--- BeautifulDnd/app/src/App.js
+++ BeautifulDnd/app/src/App.js
@@ -3,19 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import { DragDropContext } from "react-beautiful-dnd";
 import initialData from './InitialData';
-import Column from './column.jsx';
+import Column from './column';
 
 class App extends React.Component {
 	state = initialData;
 
+	onDragEnd = result => {
+		//TODO reorder list
+	}
+
 
 	render() {
-		return this.state.columnOrder.map((columnId) => {
-			const column = this.state.columns[columnId];
-			const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+		return (
+			<DragDropContext onDragEnd={this.onDragEnd}>
+				{this.state.columnOrder.map((columnId) => {
+					const column = this.state.columns[columnId];
+					const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
-			return <Column key={column.id} column={column} tasks={tasks} />;
-		});
+					return <Column key={column.id} column={column} tasks={tasks} />;
+				})}
+			</DragDropContext>
+		);
+
 	}
 }
 
--- BeautifulDnd/app/src/task.js
+++ BeautifulDnd/app/src/task.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 
 export default class Task extends React.Component {
 	render() {
-		return <div>{this.props.task.content}</div>;
+		return (
+			<Draggable draggableId={this.props.task.id} index={this.props.index}>
+				{(provided) => (
+					<div
+						ref={provided.innerRef}
+						{...provided.draggableProps}
+						{...provided.dragHandleProps}
+					>
+							<div>
+								{this.props.task.content}
+							</div>
+					</div>
+				)}
+			</Draggable>
+
+		);
 	}
 }
